0a8ef98f57d98038f6457ba2e37b60e66144db3b
KaweVK <victorkawe07@gmail.com>
2025-06-12 18:02:39 -0300

    Using dto to mapper to author and saving

--- libraryapi/src/main/java/com/github/kawevk/libraryapi/controller/AuthorController.java
+++ libraryapi/src/main/java/com/github/kawevk/libraryapi/controller/AuthorController.java
@@ -3,12 +3,14 @@ package com.github.kawevk.libraryapi.controller;
 import com.github.kawevk.libraryapi.dto.AuthorDTO;
 import com.github.kawevk.libraryapi.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/authors")
@@ -18,9 +20,17 @@ public class AuthorController {
     private AuthorService authorService;
 
     @PostMapping
-    public ResponseEntity createAuthor(@RequestBody AuthorDTO authorDTO) {
+    public ResponseEntity<Object> createAuthor(@RequestBody AuthorDTO authorDTO) {
+        var author = authorDTO.MapToAuthor();
+        authorService.createAuthor(author);
 
-        return new ResponseEntity("Author created successfully" + authorDTO, HttpStatus.CREATED);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(author.getId())
+                .toUri();
+
+        return ResponseEntity.created(location)
+                .body("Author created successfully with ID: " + author.getId());
     }
 
 
--- libraryapi/src/main/java/com/github/kawevk/libraryapi/dto/AuthorDTO.java
+++ libraryapi/src/main/java/com/github/kawevk/libraryapi/dto/AuthorDTO.java
@@ -1,6 +1,17 @@
 package com.github.kawevk.libraryapi.dto;
 
+import com.github.kawevk.libraryapi.model.Author;
+
 import java.time.LocalDate;
 
 public record AuthorDTO(String name, LocalDate birthDate, String nacionality) {
+
+    public Author MapToAuthor() {
+        Author author = new Author();
+        author.setName(name);
+        author.setBirthDate(birthDate);
+        author.setNationality(nacionality);
+        return author;
+    }
+
 }
