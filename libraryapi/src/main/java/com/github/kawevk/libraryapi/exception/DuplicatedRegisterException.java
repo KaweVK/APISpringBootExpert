80edcd7f870ecbc8e071c41cc3f09b2da7e386c7
KaweVK <victorkawe07@gmail.com>
2025-07-01 16:16:46 -0300

    New Exception

---
+++ libraryapi/src/main/java/com/github/kawevk/libraryapi/exception/DuplicatedRegisterException.java
@@ -0,0 +1,9 @@
+package com.github.kawevk.libraryapi.exception;
+
+public class DuplicatedRegisterException extends  RuntimeException {
+
+    public DuplicatedRegisterException(String message) {
+        super(message);
+    }
+
+}
