3c432e840765aa7e771cd98449b3946ddd19bb0a
KaweVK <victorkawe07@gmail.com>
2025-07-01 16:17:27 -0300

    New find Method

--- libraryapi/src/main/java/com/github/kawevk/libraryapi/repository/AuthorRepository.java
+++ libraryapi/src/main/java/com/github/kawevk/libraryapi/repository/AuthorRepository.java
@@ -3,7 +3,9 @@ package com.github.kawevk.libraryapi.repository;
 import com.github.kawevk.libraryapi.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AuthorRepository extends JpaRepository<Author, UUID> {
@@ -13,4 +15,7 @@ public interface AuthorRepository extends JpaRepository<Author, UUID> {
     List<Author> findByNationality(String nacionality);
 
     List<Author> findByNameAndNationality(String name, String nacionality);
+
+    Optional<Author> findByNationalityAndNameAndBirthDate(String nationality, String name, LocalDate birthDate);
 }
+
