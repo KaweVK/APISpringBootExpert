824c01b471ddbae56e0ffc23d48c7a48cf60beae
KaweVK <victorkawe07@gmail.com>
2025-07-01 19:07:03 -0300

    Add validation

--- libraryapi/src/main/java/com/github/kawevk/libraryapi/dto/AuthorDTO.java
+++ libraryapi/src/main/java/com/github/kawevk/libraryapi/dto/AuthorDTO.java
@@ -1,11 +1,20 @@
 package com.github.kawevk.libraryapi.dto;
 
 import com.github.kawevk.libraryapi.model.Author;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record AuthorDTO(UUID id, String name, LocalDate birthDate, String nacionality) {
+public record AuthorDTO
+        (UUID id,
+         @NotBlank(message = "Name cannot be blank")
+         String name,
+         @NotNull(message = "Birth date is required")
+         LocalDate birthDate,
+         @NotBlank(message = "Nationality cannot be blank")
+         String nacionality) {
 
     public Author MapToAuthor() {
         Author author = new Author();
