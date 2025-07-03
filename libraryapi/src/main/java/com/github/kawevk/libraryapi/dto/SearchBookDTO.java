6aa27ec84a503b96b966b262da575bec8a9f95de
KaweVK <victorkawe07@gmail.com>
2025-07-02 23:14:32 -0300

    Create SearchBookDTO

---
+++ libraryapi/src/main/java/com/github/kawevk/libraryapi/dto/SearchBookDTO.java
@@ -0,0 +1,18 @@
+package com.github.kawevk.libraryapi.dto;
+
+import com.github.kawevk.libraryapi.model.BookGender;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record SearchBookDTO(
+        UUID id,
+        String title,
+        String isbn,
+        LocalDate publishedDate,
+        BookGender gender,
+        BigDecimal price,
+        AuthorDTO author
+) {
+}
