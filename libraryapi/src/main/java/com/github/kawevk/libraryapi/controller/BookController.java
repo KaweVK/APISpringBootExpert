a6f26718efade257ea1e03aabda41f5dab2ceb79
KaweVK <victorkawe07@gmail.com>
2025-07-07 16:55:14 -0300

    Post method created

--- libraryapi/src/main/java/com/github/kawevk/libraryapi/controller/BookController.java
+++ libraryapi/src/main/java/com/github/kawevk/libraryapi/controller/BookController.java
@@ -1,7 +1,14 @@
 package com.github.kawevk.libraryapi.controller;
 
+import com.github.kawevk.libraryapi.dto.ErrorAnswer;
+import com.github.kawevk.libraryapi.dto.RegisterBookDTO;
 import com.github.kawevk.libraryapi.service.BookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     private final BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<Object> createBook(@RequestBody @Valid RegisterBookDTO book) {
+       try {
+            return ResponseEntity.ok(book);
+        } catch (Exception e) {
+            var errorDTO = ErrorAnswer.conflictAnswer(e.getMessage());
+            return ResponseEntity.status(errorDTO.status()).body(errorDTO);
+        }
+    }
 }
