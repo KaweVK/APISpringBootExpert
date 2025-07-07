f637e17a90ab31b748d92b53e09acc4ebae2771f
KaweVK <victorkawe07@gmail.com>
2025-07-07 16:57:39 -0300

    Add validates on register book dto

--- libraryapi/src/main/java/com/github/kawevk/libraryapi/dto/RegisterBookDTO.java
+++ libraryapi/src/main/java/com/github/kawevk/libraryapi/dto/RegisterBookDTO.java
@@ -1,17 +1,28 @@
 package com.github.kawevk.libraryapi.dto;
 
 import com.github.kawevk.libraryapi.model.BookGender;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record RegisterBookDTO(
-        String title,
+
+        @ISBN
+        @NotBlank(message = "Campo obrigatório")
         String isbn,
+        @NotBlank(message = "Campo obrigatório")
+        String title,
+        @NotNull(message = "Campo obrigatório")
+        @Past(message = "Data de publicação deve ser no passado")
         LocalDate publishedDate,
         BookGender gender,
         BigDecimal price,
+        @NotNull(message = "Campo obrigatório")
         UUID authorId
 ) {
 }
