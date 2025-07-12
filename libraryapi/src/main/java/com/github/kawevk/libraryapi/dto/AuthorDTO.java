4fada96e193fddf44aa846e3321dcd9c8f6ef7de
KaweVK <victorkawe07@gmail.com>
2025-07-11 23:40:58 -0300

    Change errors to portuguese

--- libraryapi/src/main/java/com/github/kawevk/libraryapi/dto/AuthorDTO.java
+++ libraryapi/src/main/java/com/github/kawevk/libraryapi/dto/AuthorDTO.java
@@ -10,13 +10,13 @@ import java.util.UUID;
 
 public record AuthorDTO
         (UUID id,
-         @NotBlank(message = "Name cannot be blank")
-         @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
+         @NotBlank(message = "Nome não pode ser vazio")
+         @Size(min = 3, max = 100, message = "Nome deve ter entre 3 e 100 caracteres")
          String name,
-         @NotNull(message = "Birth date is required")
-         @Past(message = "Birth date must be in the past")
+         @NotNull(message = "Data de nascimento é necessária")
+         @Past(message = "Data de nascimento deve ser no passado")
          LocalDate birthDate,
-         @NotBlank(message = "Nationality cannot be blank")
-         @Size(min = 2, max = 50, message= "Nationality must be between 2 and 50 characters")
+         @NotBlank(message = "Nacionalidade não pode ser vazia")
+         @Size(min = 2, max = 50, message= "Nacionalidade deve ter entre 2 e 50 caracteres")
          String nationality) {
 }
