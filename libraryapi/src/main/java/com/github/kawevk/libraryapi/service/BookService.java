78739c39d5eff8a9a5dfa6ed872333a3b754bc24
KaweVK <victorkawe07@gmail.com>
2025-07-14 19:15:41 -0300

    Created search method

--- libraryapi/src/main/java/com/github/kawevk/libraryapi/service/BookService.java
+++ libraryapi/src/main/java/com/github/kawevk/libraryapi/service/BookService.java
@@ -1,10 +1,15 @@
 package com.github.kawevk.libraryapi.service;
 
 import com.github.kawevk.libraryapi.model.Book;
+import com.github.kawevk.libraryapi.model.BookGender;
 import com.github.kawevk.libraryapi.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import static com.github.kawevk.libraryapi.repository.specs.BookSpecs.*;
+
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -24,5 +29,29 @@ public class BookService {
 
     public void deleteBook(Book book) {
         bookRepository.delete(book);
+    }
+
+    public List<Book> search(String isbn, String title, String authorName, BookGender bookGender, Integer publicationYear) {
+
+        Specification<Book> specs = Specification
+                .where( ((root, query, cb) -> cb.conjunction()) );
+
+        if (isbn != null && !isbn.isEmpty()) {
+            specs = specs.and(isbnEqual(isbn));
+        }
+        if (title != null && !title.isEmpty()) {
+            specs = specs.and(titleLike(title));
+        }
+        if (bookGender != null) {
+            specs = specs.and(bookGenderEqual(bookGender));
+        }
+        if (authorName != null && !authorName.isEmpty()) {
+            specs = specs.and(authorNameLike(authorName));
+        }
+        if (publicationYear != null) {
+            specs = specs.and(publicationYearEqual(publicationYear));
+        }
+
+        return bookRepository.findAll(specs);
     }
 }
